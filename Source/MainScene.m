19473f32e302642040485a77fa31d19cf22bf57d
aoraptor <aowongster@gmail.com>
2014-10-13 22:56:47 -0700

    velocity and rotation, responding to touch.

--- Source/MainScene.m
+++ Source/MainScene.m
@@ -7,7 +7,57 @@
 //
 
 #import "MainScene.h"
+static const CGFloat scrollSpeed = 80.f;
+@implementation MainScene {
+    CCSprite *_hero;
+    CCPhysicsNode *_physicsNode;
+    CCNode *_ground1;
+    CCNode *_ground2;
+    NSArray *_grounds;
+    NSTimeInterval _sinceTouch;
+}
 
-@implementation MainScene
+- (void)didLoadFromCCB {
+    _grounds = @[_ground1, _ground2];
+    self.userInteractionEnabled = TRUE;
+}
 
+
+- (void)update:(CCTime)delta {
+    _hero.position = ccp(_hero.position.x + delta * scrollSpeed, _hero.position.y);
+    _physicsNode.position = ccp(_physicsNode.position.x - (scrollSpeed *delta), _physicsNode.position.y);
+    
+    // clamp velocity
+    float yVelocity = clampf(_hero.physicsBody.velocity.y, -1 * MAXFLOAT, 200.f);
+    _hero.physicsBody.velocity = ccp(0, yVelocity);
+    
+    // downwards impulse on no touch
+    _sinceTouch += delta;
+    _hero.rotation = clampf(_hero.rotation, -30.f, 90.f);
+    if (_hero.physicsBody.allowsRotation) {
+        float angularVelocity = clampf(_hero.physicsBody.angularVelocity, -2.f, 1.f);
+        _hero.physicsBody.angularVelocity = angularVelocity;
+    }
+    if ((_sinceTouch > 0.5f)) {
+        [_hero.physicsBody applyAngularImpulse:-4000.f*delta];
+    }
+    
+    // ground scroll
+    for (CCNode *ground in _grounds) {
+        // get the world position of the ground
+        CGPoint groundWorldPosition = [_physicsNode convertToWorldSpace:ground.position];
+        // get the screen position of the ground
+        CGPoint groundScreenPosition = [self convertToNodeSpace:groundWorldPosition];
+        // if the left corner is one complete width off the screen, move it to the right
+        if (groundScreenPosition.x <= (-1 * ground.contentSize.width)) {
+            ground.position = ccp(ground.position.x + 2 * ground.contentSize.width, ground.position.y);
+        }
+    }
+}
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    [_hero.physicsBody applyImpulse:ccp(0, 400.f)];
+    [_hero.physicsBody applyAngularImpulse:10000.f];
+    _sinceTouch = 0.f;
+}
 @end
